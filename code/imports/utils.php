aebf316d2fd4e7c1b2e0d39bd3d4a89220957db8
ben <benjamin.james98@gmail.com>
2019-08-23 01:17:19 +1000

    Added language filtration
    - Added language_filter function in utils.php
    
    - Added blacklist.php file
    
    - Added documentation comments in utils.php for existing functions

--- code/imports/utils.php
+++ code/imports/utils.php
@@ -1,17 +1,53 @@
 <?php
 
-function get_db() {
-    $db = mysqli_connect('localhost', 'root', 'root', 'FedNews');
 
-    if (!$db) die(mysqli_connect_error());
-    else return $db;
+/**
+ * @return mysqli
+ *
+ * This function is the basic DB access function used throughout the site
+ */
+function get_db() {
+  $db = mysqli_connect('localhost', 'root', 'root', 'FedNews');
+
+  if (!$db) die(mysqli_connect_error());
+  else return $db;
 }
 
+/**
+ * @param $pass String - The password that is to be hashed
+ *
+ * @return string The hashed password
+ *
+ * This function is used in the signup process to generate hased passwords
+ * for the DB
+ */
 function get_hash($pass) {
-    $bytes = openssl_random_pseudo_bytes(30);
-    $random_data = substr(base64_encode($bytes), 0, 22);
-    $random_data = strtr($random_data, '+', '.');
+  $bytes = openssl_random_pseudo_bytes(30);
+  $random_data = substr(base64_encode($bytes), 0, 22);
+  $random_data = strtr($random_data, '+', '.');
 
-    $local_salt = "$2y$12$" . $random_data;
-    return crypt($pass, $local_salt);
+  $local_salt = "$2y$12$" . $random_data;
+  return crypt($pass, $local_salt);
+}
+
+
+/**
+ * @param $string    String - The input to be filtered. Can be any case (upper
+ *                   or lower).
+ * @param $blacklist array - The list of words to be filtered out. All words
+ *                   should be lowercase.
+ *
+ * @return array
+ *
+ * This function is used to parse incoming news stories. It returns an array
+ * containing banned words which appear in the text
+ */
+function filter_language($string, $blacklist) {
+  $words = explode(' ', strtolower($string));
+  $found = [];
+  foreach ($blacklist as $item)
+    if (in_array($item, $words)) $found[] = $item;
+
+
+  return $found;
 }
